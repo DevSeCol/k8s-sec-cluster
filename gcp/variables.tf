e46f84a3e48017da4c67f1ff9a841c381915b4f9
ryepesg <ryepesg@users.noreply.github.com>
2020-07-05 01:24:47 +1000

    Reducing the number of variables
    They should be outputs of other modules

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -6,26 +6,6 @@ variable "region" {
   type = string
 }
 
-variable "zone" {
-  type = string
-}
-
 variable "cluster_name" {
-  type = string
-}
-
-variable "cluster_network" {
-  type = string
-}
-
-variable "cluster_subnetwork" {
-  type = string
-}
-
-variable "machine_type" {
-  type = string
-}
-
-variable "service_account" {
   type = string
 }
